b37084fb83132763f0efee778781a578b0adec51
AASHISH0709 <75165408+AASHISH0709@users.noreply.github.com>
2020-11-28 17:03:21 +0530

    Add files via upload

---
+++ LL.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include <stdlib.h>
+using namespace std;
+
+
+struct node{
+	int data;
+	struct node *next;
+};
+
+void traversal(struct node *ptr)
+{
+	while(ptr != NULL){
+	cout<<ptr->data<<endl;
+	ptr = ptr ->next;
+	}
+
+}
+
+int main()
+{
+struct node * head;
+struct node * sec;
+struct node * thr;
+head = (struct node *) malloc(sizeof(struct node));
+sec = (struct node *) malloc(sizeof(struct node));
+thr = (struct node *) malloc(sizeof(struct node));
+
+head->data = 7;
+head->next = sec;
+
+sec->data = 70;
+sec->next = thr;
+
+thr->data = 90;
+thr->next = NULL;
+
+traversal(head);
+
+return 0;
+}
